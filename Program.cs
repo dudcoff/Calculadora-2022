c9e7fa0f70b1ddbf76bf4bfd2f0de95ee016bd9d
Maria Eduarda Casalunga <mariaeduardacasalunga@gmail.com>
2022-05-15 22:59:55 -0300

    Subrotinas para funções

--- Program.cs
+++ Program.cs
@@ -15,22 +15,20 @@ opcaoDesejada = Console.ReadLine()!;
 
 switch (opcaoDesejada)
 {
-    case "+":
-        CalcularSoma();
+     case "+":
+        Calculadora.CalcularSoma();
         break;
-
     case "-":
-        CalcularSubtracao();
+        Calculadora.CalcularSubtracao();
         break;
-
     case "*":
-        CalcularMultipicacao();
+        Calculadora.CalcularMultiplicacao();
         break;
     case "/":
-        CalcularDivisao();
+        Calculadora.CalcularDivisao();
         break;
     default:
-    Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.ForegroundColor = ConsoleColor.DarkRed;
         Console.WriteLine("Opção inválida.");
         break;
 }
@@ -38,81 +36,3 @@ switch (opcaoDesejada)
 Console.ForegroundColor = ConsoleColor.Magenta;
 Console.WriteLine("\nObrigado por utilizar nosso programa <3 .");
 Console.ResetColor();
-
-void CalcularSoma()
-{
-     int n1, n2, resultado;
-        Console.WriteLine("--- Soma ---");
-
-        Console.Write("Digite o primeiro número: ");
-        n1 = Convert.ToInt32(Console.ReadLine()!); 
-
-        Console.Write("Digite o segundo número: ");
-        n2 = Convert.ToInt32(Console.ReadLine()!);
-
-        resultado = n1 + n2;
-         Console.WriteLine($"{n1} somado por {n2} é {resultado}");
-
-        Console.WriteLine("\nPressione uma tecla para continuar...");
-        Console.ReadKey();
-    return;
-}
-
-void CalcularSubtracao()
-{
-    int n1, n2, resultado;
-        Console.WriteLine("--- Subtração ---");
-
-        Console.Write("Digite o primeiro número: ");
-        n1 = Convert.ToInt32(Console.ReadLine()!); 
-
-        Console.Write("Digite o segundo número: ");
-        n2 = Convert.ToInt32(Console.ReadLine()!);
-
-        resultado = n1 - n2;
-         Console.WriteLine($"{n1} subtraido por {n2} é {resultado}");
-
-        Console.WriteLine("\nPressione uma tecla para continuar...");
-        Console.ReadKey();
-    return;
-}
-
-void CalcularMultipicacao()
-{
-    double a, b, resultado;
-
-        Console.WriteLine("--- Multiplicação A*B ---\n");
-        Console.WriteLine("Digite os valores.");
-
-        Console.Write("A = ");
-        a = Convert.ToDouble(Console.ReadLine());
-        Console.Write("B = ");
-        b = Convert.ToDouble(Console.ReadLine());
-
-        resultado = a * b; 
-        Console.WriteLine($"{a} multiplicado por {b} é {resultado}");
-
-        Console.WriteLine("\nPressione uma tecla para continuar...");
-        Console.ReadKey();
-    return;
-} 
-
-void CalcularDivisao()
-{
-    double a, b, resultado;
-
-        Console.WriteLine("--- Divisão ---\n");
-        Console.WriteLine("Digite os valores.");
-
-        Console.Write("A = ");
-        a = Convert.ToDouble(Console.ReadLine());
-        Console.Write("B = ");
-        b = Convert.ToDouble(Console.ReadLine());
-
-        resultado = a / b; 
-        Console.WriteLine($"{a} dividido por {b} é {resultado}");
-
-        Console.WriteLine("\nPressione uma tecla para continuar...");
-        Console.ReadKey();
-    return;
-}
